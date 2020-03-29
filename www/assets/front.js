dbd8d77e8cd00bab8e754bc28ae1f58f26941aaa
Tomas Reinert <reinerttomas@gmail.com>
2020-03-29 15:39:56 +0200

    front template

--- www/assets/front.js
+++ www/assets/front.js
@@ -4,11 +4,25 @@
 import 'jquery';
 import 'bootstrap';
 import 'owl.carousel';
-import './js/owlcarousel-custom';
+import 'isotope-layout';
+import 'waypoints/src/waypoint';
+import 'imagesloaded';
+import 'magnific-popup/dist/jquery.magnific-popup'
+import 'gijgo';
+import './front/js/jquery.slicknav.min';
+import './front/js/main';
 
-import './css/font.front.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+/**
+ * Css
+ */
+import 'bootstrap/dist/css/bootstrap.css';
 import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-import './css/theme.css';
-import './css/front.css';
+import 'magnific-popup/dist/magnific-popup.css'
+import './front/css/font-awesome.min.css';
+import './front/css/themify-icons.css';
+import './front/css/flaticon.css';
+import 'animate.css/animate.css';
+import './front/css/slick.css';
+import './front/css/slicknav.css';
+import './front/css/style.css';
+import './front/css/custom.css';
